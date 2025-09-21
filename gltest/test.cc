a087bae31faecb654139f64da0d020ec03f6e430
feifei-111 <2364819892@qq.com>
2025-09-21 08:01:20 +0800

    update

--- gltest/test.cc
+++ gltest/test.cc
@@ -74,36 +74,18 @@ int main()
         1, 2, 3  // second triangle
     };
 
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    
-    // start record
-    glBindVertexArray(VAO);
+    fg_gl::VAO vao;
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+    fg_gl::VBO<float> vbo(vertices, 8, 4, GL_FLOAT, GL_STATIC_DRAW, 0);
+    vbo.SetAttr(3, GL_FALSE);
+    vbo.SetAttr(3, GL_FALSE);
+    vbo.SetAttr(2, GL_FALSE);
+    fg_gl::EBO ebo(indices, sizeof(indices), GL_STATIC_DRAW);
 
     // end record
-    glBindVertexArray(0); 
-
-    // 在完成记录之后解绑
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); 
-
+    vao.UnBind();
+    vbo.UnBind();
+    ebo.UnBind();
 
 
     // render loop
@@ -121,13 +103,13 @@ int main()
         float timeValue = glfwGetTime();
         float greenValue = sin(timeValue) / 2.0f + 0.5f;
 
+        vao.Bind();
         program.Use();
 
         program.UniformFloat3("offset", 0.0f, -0.0f, 0.0f);
         program.UniformInt("Tex0", tex0.CurrentUnit());
         program.UniformInt("Tex1", tex1.CurrentUnit());
 
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
  
@@ -137,13 +119,6 @@ int main()
         glfwPollEvents();
     }
 
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
@@ -160,7 +135,5 @@ void processInput(GLFWwindow *window)
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
--- src/fg_gl/gl.h
+++ src/fg_gl/gl.h
@@ -15,6 +15,7 @@ extern "C"{
 #endif
 
 #include <string>
+#include<iostream>
 
 namespace fg_gl{
 
@@ -48,6 +49,88 @@ private:
     int width_;
     int height_;
     int channel_;
+};
+
+template <typename Dtype>
+struct VBO{
+    VBO(
+        Dtype* data, 
+        unsigned int column_num, 
+        unsigned int vertex_num, 
+        unsigned int gl_dtype, 
+        unsigned int draw_type, 
+        unsigned int attr_begin=0)
+        :attr_idx_(attr_begin), column_num_(column_num), vertex_num_(vertex_num), gl_dtype_(gl_dtype), column_idx_(0)
+    {
+        stride_ = sizeof(Dtype) * column_num_;
+        size_t data_size = stride_ * vertex_num_;
+        glGenBuffers(1, &vbo_);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+        glBufferData(GL_ARRAY_BUFFER, data_size, data, draw_type);
+    }
+
+    void SetAttr(int column_size, unsigned int norm){
+        glVertexAttribPointer(attr_idx_, column_size, gl_dtype_, norm, stride_, (void*)column_idx_);
+        glEnableVertexAttribArray(attr_idx_);
+        attr_idx_++;
+        column_idx_ += column_size;
+    }
+    void Bind(){
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+    }
+
+    void UnBind(){
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    ~VBO(){
+        UnBind();
+        glDeleteBuffers(1, &vbo_);
+    }
+private:
+    unsigned int vbo_;
+    int attr_idx_;
+    Dtype* column_idx_;
+    int column_num_;
+    int vertex_num_;
+    unsigned int gl_dtype_;
+    size_t stride_;
+};
+
+struct EBO{
+    EBO(unsigned int* data, unsigned int data_size, unsigned int draw_type){
+        glGenBuffers(1, &ebo_);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, data_size, data, draw_type);
+    }
+    void Bind(){
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo_);
+    }
+    void UnBind(){
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    }
+    ~EBO(){
+        glDeleteBuffers(1, &ebo_);
+    }
+private:
+    unsigned int ebo_;
+};
+
+struct VAO{
+    VAO(){
+        glGenVertexArrays(1, &vao_);
+        glBindVertexArray(vao_);
+    }
+    void Bind(){
+        glBindVertexArray(vao_);
+    }
+    void UnBind(){
+        glBindVertexArray(0);
+    }
+    ~VAO(){
+        glDeleteVertexArrays(1, &vao_);
+    }
+private:
+    unsigned int vao_;
 };
 
 }
